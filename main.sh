590902c6ad59884d8e759ff0d7c45f450323ef7b
elroykanye <elroykanye@gmail.com>
2021-11-07 00:57:17 +0100

    initialise script: modified ./main.sh

---
+++ main.sh
@@ -0,0 +1,47 @@
+#!/bin/sh
+#
+# SCRIPT: Main
+# AUTHOR: Elroy Kanye
+# DATE: 07 November 2021
+# 1.1.A (Valid are A, B, D, T and P)
+#       (For Alpha, Beta, Dev, Test and Production)
+#
+# PLATFORM: Linux
+#
+# PURPOSE: Main entry script for this project.
+#
+# REV LIST:
+#       DATE: 08 November 2021
+#       By: Elroy Kanye
+#       Modification: Initialisation
+#
+# set -n    # Uncomment to check script syntax, without execution.
+#           # NoteL Do not forget to put comment back.
+# set -x    # Uncomment to debug this shell script
+#
+############################################################
+#       DEFINE FILES AND VARIABLES HERE
+############################################################
+
+
+
+
+############################################################
+#       DEFINE FUNCTIONS HERE
+############################################################
+
+
+
+############################################################
+#       BEGINNING OF MAIN
+############################################################
+
+
+
+
+
+
+
+############################################################
+#       END OF SCRIPT
+############################################################
