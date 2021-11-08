e8c7c9c836d2113da3e8eb92a8ac94470870c0b7
elroykanye <elroykanye@gmail.com>
2021-11-08 04:42:08 +0100

    completed fn for setting up dirs

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,18 @@
 use std::collections::HashMap;
 use std::process::Command;
+use std::env;
 
 fn main() {
-    println!("Hello, world!");
+    let args: Vec<String> = env::args().collect();
+    println!("Creating required directories!");
+    
+    let target_dir: &String= &args[1];
 
-    setup_dirs();
+    setup_dirs(target_dir);
     
 }
 
-fn setup_dirs() {
+fn setup_dirs(target_dir: &String) {
     let mut package_dirs: HashMap<String, Vec<&str>> = HashMap::new();
     package_dirs.insert("api".to_string(), Vec::from(["controller", "dto"]));
     package_dirs.insert("business".to_string(), Vec::from(["mapper", "service"]));
@@ -16,14 +20,25 @@ fn setup_dirs() {
     package_dirs.insert("data".to_string(), Vec::from(["entity", "repository"]));
     package_dirs.insert("exception".to_string(), Vec::from(["advice", "dto"]));
 
+    let mut mkdir = Command::new("mkdir").arg(target_dir).output().expect("failed to execute process");
+    
     for (parent, children) in package_dirs.iter() {
         println!("Currently working with: {}", parent);
 
-        let output = Command::new("mkdir")
-            .arg()
-
-        for el in children {
-            println!("{}", el);
+        let dir: String = target_dir.to_owned() + "/" + parent;
+        
+        mkdir = Command::new("mkdir")
+            .arg(dir.clone())
+            .output()
+            .expect("failed to execute process");
+        
+        for child in children {
+            let child_dir = dir.to_owned() + "/" + child; 
+            mkdir = Command::new("mkdir")
+            .arg(child_dir)
+            .output()
+            .expect("failed to execute process");
+        
         }
     }
 
