68db3d33e51502e7f668072fb83fa3173905fd42
elroykanye <elroykanye@gmail.com>
2021-11-08 01:56:31 +0100

    modified to match config file styling

--- global.sh
+++ global.sh
@@ -3,21 +3,10 @@
 # SCRIPT: Global functions and variables
 # AUTHOR: Elroy Kanye
 # DATE: 07 November 2021
-# 1.1.A (Valid are A, B, D, T and P)
-#       (For Alpha, Beta, Dev, Test and Production)
 #
 # PLATFORM: Linux
 #
-# PURPOSE: Generates the controller class and contains templates for it.
-#
-# REV LIST:
-#       DATE: 08 November 2021
-#       By: Elroy Kanye
-#       Modification: Initialisation
-#
-# set -n    # Uncomment to check script syntax, without execution.
-#           # NoteL Do not forget to put comment back.
-# set -x    # Uncomment to debug this shell script
+# PURPOSE: Global config script.
 #
 ############################################################
 #       DEFINE FILES AND VARIABLES HERE
